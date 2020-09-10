b1b60e29779d69a763d10150c201c0b3086c64f7
myleraghu <55377373+myleraghu@users.noreply.github.com>
2020-09-10 19:09:33 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version  = "~> 0.12"
   backend "s3" {
-    bucket = "terraform-state"
+    bucket = "terraform-state-demotest"
     key    = "sample/terraform.tfstate"
     region = "us-east-1"
   }
